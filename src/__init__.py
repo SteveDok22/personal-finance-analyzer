5b41cf90da812abb4ed61263c271de6cc7c942fa
Stiven Ntoktorov <s.doktorov123@gmail.com>
2025-10-29 16:48:12 +0100

    feat: create src package initialization
    - Add __init__.py to make src a Python package
    - Set package version to 1.0.0
    - Add package description and metadata
    - Enable proper module imports

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Personal Finance Survey Analyzer - Source Package
+
+This package contains the core modules for analyzing personal finance survey data.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Finance Analyzer Developer"
